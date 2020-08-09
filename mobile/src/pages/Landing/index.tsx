2e9c547d1d3d75d97f93f0b465998a29f1fb1b02
douglasdsda <douglasdsda@gmail.com>
2020-08-09 14:25:56 -0300

    mobile dar aula

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Image, View, Text, ScrollView } from "react-native";
+import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
+
+import landingImg from "../../assets/images/landing.png";
+import studyIcon from "../../assets/images/icons/study.png";
+import giveClasses from "../../assets/images/icons/give-classes.png";
+import heartIcon from "../../assets/images/icons/heart.png";
+import { RectButton } from "react-native-gesture-handler";
+
+function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassePage() {
+    navigate("GiveClasses");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo, {"\n"}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
+
+          <Text style={styles.buttonText}>Estudar</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={handleNavigateToGiveClassePage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClasses} />
+
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de 286 conexões já realizadas <Image source={heartIcon} />
+      </Text>
+    </View>
+  );
+}
+
+export default Landing;
