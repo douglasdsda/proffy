f28485353ab6a34ef7715a6c14cd2d61920774ae
douglasdsda <douglasdsda@gmail.com>
2020-08-15 22:02:21 -0300

    v2

--- web/src/pages/Signin/index.tsx
+++ web/src/pages/Signin/index.tsx
@@ -1,45 +1,51 @@
 import React, { FormEvent, useCallback, useState } from "react";
 import { Link } from "react-router-dom";
  
-import logo from "../../assets/images/success-background.svg";
- 
+import background from "../../assets/images-v2/Proffy.png";
+
 import InputUser from "../../components/InputUser";
 
 import "./styles.css";
 
 const Signin: React.FC = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
+  const handleSignin = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
 
-  const handleSignin = useCallback((e: FormEvent)=> {
-
-    e.preventDefault();
-
-    console.log({
-      name, password
-    });
-
-  }, [name, password])
+      console.log({
+        name,
+        password,
+      });
+    },
+    [name, password]
+  );
 
   return (
     <div id="page-signin">
       <div className="container" id="page-signin-content">
         <div className="background-container">
-           
+          <img src={background} alt="logo"/>
         </div>
 
         <div className="form-content">
-          <form onSubmit={handleSignin} >
+          <form onSubmit={handleSignin}>
             <h1>Fazer login</h1>
             <InputUser
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            name="name" placeholder="Nome" />
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              name="name"
+              placeholder="Nome"
+            />
             <InputUser
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)}
-            name="password" typePassword placeholder="Senha" />
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              typePassword
+              placeholder="Senha"
+            />
 
             <div className="form-option-content">
               <span className="form-option-save">
@@ -53,14 +59,15 @@ const Signin: React.FC = () => {
             <button name="submit" type="submit">
               Entrar
             </button>
+
+            <div className="form-footer">
+              <div className="form-footer-register">
+                <span>Não tem conta?</span>
+                <Link to="/">Cadastre-se</Link>
+              </div>
+              <span>É de graça</span>
+            </div>
           </form>
-        </div>
-        <div className="form-footer">
-          <div className="form-footer-register">
-            <span>Não tem conta?</span>
-            <Link to="/">Cadastre-se</Link>
-          </div>
-          <span>É de graça</span>
         </div>
       </div>
     </div>
