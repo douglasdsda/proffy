0be2486883d04f71b55b4ea8e269b6c630a08567
douglasdsda <douglasdsda@gmail.com>
2020-08-19 10:34:30 -0300

    server

--- server/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ server/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository } from 'typeorm';
-import IUsersRepository from 'modules/users/repositories/IUsersRepository';
-import ICreateUserDTO from 'modules/users/dtos/ICreateUserDTO';
+import IUsersRepository from '../../../repositories/IUsersRepository';
+import ICreateUserDTO from '../../../dtos/ICreateUserDTO';
 import User from '../entities/User';
 
 class UsersRepository implements IUsersRepository {
