2e7b5c3ed12c1334862c758e3a865b403e40e899
douglasdsda <douglasdsda@gmail.com>
2020-09-06 11:37:21 -0300

    server funcionando

--- server/babel.config.js
+++ server/babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript'
+    "presets": [
+        ["@babel/preset-env", { targets: { node: "current" } }],
+        "@babel/preset-typescript"
     ],
-    plugins: [
-        'babel-plugin-transform-typescript-metadata',
-        ["@babel/plugin-proposal-decorators", { "legacy" : true},]
-        ["@babel/plugin-proposal-class-properties", { "loose" : true }]
-    ]
-}
+    "plugins": [
+        "babel-plugin-transform-typescript-metadata",
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      ],
+} 
--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -1,34 +1,40 @@
-import { Request, Response, NextFunction } from "express";
-import jwt, { verify } from "jsonwebtoken";
-import authConfig from "../config/auth";
+import { Request, Response, NextFunction } from 'express';
+import jwt, { verify } from 'jsonwebtoken';
+import AppError from '../errors/AppErros';
+import authConfig from '../config/auth';
 
 interface TokenPayload {
-  iat: number;
-  exp: number;
-  sub: number;
+    iat: number;
+    exp: number;
+    sub: number;
 }
 
-export = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+export default function ensureAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): void {
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader) return res.status(401).json({ error: "token is empty." });
+    if (!authHeader) {
+        throw new Error('JWT token is missing');
+    }
 
-  const [, token] = authHeader.split(" ");
+    const [, token] = authHeader.split(' ');
 
-  try {
-    const decoded = verify(token, authConfig.jwt.secret);
+    try {
+        const decoded = verify(token, authConfig.jwt.secret);
 
-    const { sub } = decoded as TokenPayload;
+        const { sub } = decoded as TokenPayload;
 
-    jwt.verify(token, authConfig.jwt.secret);
- 
-    req.user = {
-      id: sub,
-    };
- 
+        jwt.verify(token, authConfig.jwt.secret);
 
-    return next();
-  } catch (err) {
-    return res.status(401).json({ error: "Token is invalid." });
-  }
-};
+        req.user = {
+            id: sub,
+        };
+
+        return next();
+    } catch (error) {
+        throw new AppError('Invalid JWT token', 401);
+    }
+}
